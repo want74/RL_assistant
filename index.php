0ced03c79602bbdaa423b681c312c34dfbbdbdd8
K4N <andrushkakychkin@gmail.com>
2022-01-21 08:09:36 +0900

    qr сделан

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+	include('qr/phpqrcode/qrlib.php');
+    
+    // outputs image directly into browser, as PNG stream
+     $ourParamId = 1234;
+    
+    echo '<img src="qr.php?id='.$ourParamId.'" />';      ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+ <body>
+     
+ </body>
+ </html>
