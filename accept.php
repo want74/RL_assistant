91984cd874b2cdf86b765e8010316e4cfb33287b
K4N <want74@rambler.ru>
2022-03-03 23:12:27 +0900

    Тайтлы

--- accept.php
+++ accept.php
@@ -21,7 +21,7 @@ $query1 = mysqli_query($con, "INSERT INTO history (type, day1, user_id, id) VALU
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>RLchik</title>
 	
 	<style>
 		body {
